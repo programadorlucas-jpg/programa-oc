85cd5dc56e130ea0f09f49063fadb750a62ae093
programadorlucas-jpg <rocklucas882@gmail.com>
2026-02-10 22:48:43 +0000

    Manipulação-de-variáveis.c

---
+++ Manipulação-de-variáveis.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+    int x = 5.5;
+    int y = 2,2;
+    int soma = x + y;
+    int diferencial = x - y;
+    int produto = x * y;
+    int quociente = x / y;
+
+    printf ("soma: %d/n", soma);
+    printf ("difencial: %d/n", diferencial);
+    printf ("produto: %d/n", produto);
+    printf ("quociente: %d/n", quociente);
+
+    int a = 10;
+    float b = 3.5;
+    float resultado = a + b;
+
+    printf("resultado: %.2f/n", resultado);
+
+    return 0:
+
+
+
+}
