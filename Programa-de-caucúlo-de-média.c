38fea50f68cfd82de4cf64e5621aefb30d338814
programadorlucas-jpg <rocklucas882@gmail.com>
2026-02-11 22:02:39 +0000

    Programa-de-cáuculo-de-média.c

---
+++ Programa-de-caucúlo-de-média.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main (){
+    int nota1, nota2, nota3;
+    int media;
+
+    printf("*** Programa de cáuculo de média *** /n");
+
+    printf("Digite sua primeira mota: /n"):
+    scanf("%b", &nata1);
+
+    printf("Digite sua segunda mota: /n"):
+    scanf("%b", &nata2);
+
+    printf("Digite sua terceira mota: /n"):
+    scanf("%b", &nata3);
+
+    media = (nata1 + nota2 + nota3) /3;
+
+    print ("A média e: %d");
+
+
+
+
+
+}
