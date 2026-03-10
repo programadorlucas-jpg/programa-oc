79083c67ca6c6f1145e9da688c14ea40d1ee79dc
programadorlucas-jpg <rocklucas882@gmail.com>
2026-03-10 23:35:45 +0000

    operadores.c

---
+++ operador-matématico.c/Operadores-matemáticos.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+       
+int main(){
+
+// Soma (+), Subitração (-), Multiplicação (*), Divisão (/) 
+
+
+
+int numero1, numero2;
+int soma, subitraçao,multiplicaçao, divisao
+
+printf("Entre com o numero1: \n")
+scanf ("%d", &numero1)
+printf("Entre com o numero2: \n")
+scanf ("%d", &numero2)
+
+soma = numero1 + numero2: 
+subitraçao = numero1 - numero2: 
+multiplicaçao = numero1 * numero2: 
+divisao = numero1 / numero2: 
+
+printf("A soma é: %d\n" soma);
+printf("A subitraçao é: %d\n" subitraçao);
+printf("A multiplicaçao é: %d\n" multiplicaçao);
+printf("A divisao é: %d\n" divisao);
+
+retorn0
+ }
+
